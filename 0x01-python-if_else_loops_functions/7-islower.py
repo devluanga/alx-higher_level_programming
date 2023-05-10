c822d62eb7bd94a0d604029afe1a4a33d3a0e5c7
devluanga <imaniimaniluanga@gmail.com>
2023-05-10 14:12:25 +0000

    check if character is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""the function checks for a lowecase character
-   rototype: def islower(c):
-   Returns True if c is lowercase
-   Returns False otherwise
-   You are not allowed to import any module
-   You are not allowed to use str.upper() and str.isupper()
-"""
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return True
