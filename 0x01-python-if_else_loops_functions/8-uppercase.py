59dee1357188aba98bb88b106dc1e513f138197e
devluanga <imaniimaniluanga@gmail.com>
2023-05-10 14:35:36 +0000

    prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(c), end="")
     print("")
