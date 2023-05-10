d92c261c5c128090cba3b99b3424675e04a344f2
devluanga <imaniimaniluanga@gmail.com>
2023-05-10 13:56:44 +0000

    check if character is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -7,7 +7,7 @@
    You are not allowed to use str.upper() and str.isupper()
 """
 def islower(x):
-    if ord(x) > 97 and ord(x) < 122:
+    if ord(x) >= 97 and ord(x) <= 122:
         return True
     else:
         return False
