0ac1364b036fff704a6d1a6318ff3a2525a1a88f
devluanga <imaniimaniluanga@gmail.com>
2023-05-11 12:05:46 -0700

    add all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    """val = len(sys.argv) - 1"""
-    count = 0
-
+    val = 0
     for x in range(len(sys.argv) - 1):
-	    count += int(sys.argv[x + 1])
-    print("{}".format(count))
+        val += int(sys.argv[x + 1])
+    print("{}".format(val))
