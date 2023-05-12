37fb76bf6fd59e2e47b4c637acabfab75750ba3b
devluanga <imaniimaniluanga@gmail.com>
2023-05-11 19:47:30 -0700

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
-from calculator_1 import *
-import sys
-
 if __name__ == "__main__":
+
+    from calculator_1 import *
+    import sys
+
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    calops = {"+": add, "-": sub, "*": mul, "/": div }
+    calops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(calops.keys()):
-	    print("Unknown operator. Available operators: +, -, * and /")
-	    sys.exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, calops[sys.argv[2]](a, b)))
